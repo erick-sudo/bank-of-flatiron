fd6eb9d2548b016bfc5f23ae00630ef2ff5b0eda
Erick Ochieng <erickochieng766@gmail.com>
2023-01-14 12:33:20 +0300

    Headings of Bank of Flatiron

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
+import "./App.css"
+
 
 function App() {
+
+  const [transactions, setTransaction] = useState([]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1 className="heading">Bank of Flatiron</h1>
+    <p className="welcome">Welcome to the Bank of Flatiron, where you can trust us with all your financial data! Use the below gif as an example of how the app should function.</p>
+    <button type="button" className="big-button">The Royal Bank of Flatiron</button>
     </div>
   );
 }
